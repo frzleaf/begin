25984bd1d180bed67d205a15595e2e234a647742
frzleaf <stash.leth@gmail.com>
2016-06-23 11:21:39 +0700

    Touch bash files

--- bash/awk1.sh
+++ bash/awk1.sh
@@ -1,1 +1,6 @@
-history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | head -n10
+1. history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | sort -nr | head -n10 | column -c3 -s " " -t 
+
+
+
+
+
