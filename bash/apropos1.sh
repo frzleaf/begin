a484cd721c82f5ebf00140fcba85f1b048e89891
Stun <stash.leth@gmail.com>
2016-06-28 23:15:44 +0700

    Add grep cmds

---
+++ bash/apropos1.sh
@@ -0,0 +1,5 @@
+1.
+apropos -s 1 partition
+
+2.
+apropos -es 5 filesystem
--- bash/grep1.sh
+++ bash/grep1.sh
@@ -3,3 +3,16 @@ grep -rwn word dir
 
 $ grep -rwn "celine dion" ~/musics
 
+2.
+
+$ grep -ivc 'root' /etc/passwd
+
+3.
+
+$ rgrep -ci linux /etc | awk -F: '{ if ($NF) print $1 }'
+
+4.
+$ fgrep -vc root /etc/passwd
+
+5.
+$ egrep 'root|ROOT' /etc/passwd
